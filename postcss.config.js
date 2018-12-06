a3302596d46e01f7ae09ebd655d08e737a99ac92
Ben van den Ende <ben@Erasmus.local>
2018-12-06 16:55:10 +0100

    mitcom

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
+    require("postcss-import"),
     // require('postcss-nested'),
-    require('postcss-cssnext')({ features: { customProperties: false } }),
-    require('cssnano'),
-    require('postcss-banner')({
-      banner: 'Theme Name: Nausikaä \nTheme URI: http://mibdw.club/nausikaa \nAuthor: MIBDW \n Author URI: http://turbo.church'
+    require("postcss-cssnext")({ features: { customProperties: false } }),
+    require("cssnano"),
+    require("postcss-banner")({
+      banner:
+        "Theme Name: Nausikaä \nTheme URI: http://mibdw.club/nausikaa \nAuthor: MIBDW \n Author URI: http://turbo.church"
     })
   ]
-}
+};
